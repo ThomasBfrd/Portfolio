b4743f582e67f926bb4fee766afc021e29b46c16
ThomasBfrd <thomas.bfrd@gmail.com>
2026-01-26 15:34:24 +0100

    feat: mise à jour des vues en récupérant la donnée via injection

--- src/views/contact/About.vue
+++ src/views/contact/About.vue
@@ -1,4 +1,11 @@
 <script setup lang="ts">
+import type { Portfolio } from '@/shared/interfaces/portfolio.interface';
+import type { Profile } from '@/shared/interfaces/profile.interface';
+import { type Ref, inject, type ComputedRef, computed } from 'vue';
+
+const portfolioData: Ref<Portfolio> | undefined = inject("portfolioData");
+const profileData: ComputedRef<Profile | undefined> = computed<Profile | undefined>(() => portfolioData?.value?.profile);
+
 </script>
 
 <template>
@@ -6,15 +13,7 @@
         <section class="p-4" data-testid="about-section">
             <h2 class="text-2xl font-bold mb-4">A propos</h2>
             <div class="flex flex-col justify-start items-start gap-4 mb-4">
-                <p>Je suis Thomas Bouffard, développeur web fullstack.</p>
-                <p>Développeur web avec deux années d’expérience en alternance sur un SaaS de facturation et de
-                    comptabilité chez EBP (groupe Cegid), j’ai travaillé sur des applications Angular / .NET en
-                    production, aussi bien sur le développement de fonctionnalités que sur l’investigation et la
-                    résolution d’incidents via le système de tickets.</p>
-
-                <p>En parallèle, je développe un projet personnel fullstack en production (React / .NET), me permettant
-                    de consolider mes compétences backend, notamment sur l'architecture en DDD.</p>
-                <p>En dehors du développement, je suis passionné par la musique électronique et le sound-design, et je compose de la musique à mes heures perdues.</p>
+                <p v-for="bio in profileData?.bio">{{ bio }}</p>
             </div>
         </section>
     </div>
--- src/views/projects/Projects.vue
+++ src/views/projects/Projects.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import Activities from '@/shared/components/activities/activities.vue';
-import { WORKS } from '@/shared/const/works';
+import type { Activity } from '@/shared/interfaces/activity.interface';
+import type { Portfolio } from '@/shared/interfaces/portfolio.interface';
+import { type Ref, inject, type ComputedRef, computed } from 'vue';
+
+const portfolioData: Ref<Portfolio> | undefined = inject("portfolioData");
+const worksData: ComputedRef<Activity[]> = computed<Activity[]>(() => portfolioData?.value?.works ?? []);
+
 </script>
 
 <template>
     <section class="p-4" data-testid="projects-section">
         <h2 class="text-2xl font-bold mb-6 text-foreground">PROJETS</h2>
-        <Activities :activities="WORKS" data-testid="work-activities" />
+        <Activities :activities="worksData" data-testid="work-activities" />
     </section>
 </template>
